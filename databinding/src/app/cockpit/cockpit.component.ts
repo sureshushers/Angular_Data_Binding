1bb28daa12a1013f96feceef96493ede97740139
sureshushers <sureshbhabu.gl@gmail.com>
2023-08-21 21:19:12 -0400

    4 files modified

--- databinding/src/app/cockpit/cockpit.component.ts
+++ databinding/src/app/cockpit/cockpit.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-cockpit',
   templateUrl: './cockpit.component.html',
-  styleUrls: ['./cockpit.component.css']
+  styleUrls: ['./cockpit.component.css'],
 })
 export class CockpitComponent {
+  newServerName = '';
+  newServerContent = '';
   
+  onAddServer() {
+    alert('Server is clicked');
+  }
+
+  onAddBluePrint() {
+    alert('Blueprint is clicked');
+  }
 }
