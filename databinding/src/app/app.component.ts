4db9eae402a95861097d897a275b73d9302dd4a8
sureshushers <sureshbhabu.gl@gmail.com>
2023-08-21 21:41:10 -0400

    4 files modified

--- databinding/src/app/app.component.ts
+++ databinding/src/app/app.component.ts
@@ -3,9 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  serverElements = [];
-
+  serverElements = [
+    { type: 'server', name: 'Potomac', content: 'Batman' },
+    { type: 'blueprint', name: 'Michigan', content: 'Joker' },
+  ];
 }
--- databinding/src/app/servers/servers.component.ts
+++ databinding/src/app/servers/servers.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent {
+  //To make the below element visible in other component, make it visible using Input() decorator.
+  @Input() element!: { type: string; name: string; content: string };
 
+  constructor() {}
 }
